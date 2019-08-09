10b5df14f06638ba6233bb730a85d8e908fbf1ac
Cesar Aponte <cesar.aponte.1996.vazquez@gmail.com>
2019-08-09 14:16:11 -0400

    mvp met. ready to deploy

--- database.js
+++ database.js
@@ -1,32 +1,42 @@
 const data = [
   {
     name: "Gary",
+    id: 1,
     chores: [
       {
         id: 1,
         description: "wash the car",
-        notes: "wax on, wax off"
+        notes: "wax on, wax off",
+        completed: true,
+        assignedTo: 1
       },
       {
         id: 2,
         description: "water the plants",
-        notes: "drowning is bad"
+        notes: "drowning is bad",
+        completed: false,
+        assignedTo: 1
       }
     ]
   },
 
   {
     name: "James",
+    id: 2,
     chores: [
       {
         id: 1,
         description: "take out the trash",
-        notes: "upstairs too"
+        notes: "upstairs too",
+        completed: false,
+        assignedTo: 2
       },
       {
         id: 2,
         description: "clean bedroom",
-        notes: "dont hide everything under the bed"
+        notes: "dont hide everything under the bed",
+        completed: true,
+        assignedTo: 2
       }
     ]
   }
--- index.js
+++ index.js
@@ -4,29 +4,77 @@ const data = require("./database");
 server.use(express.json());
 
 server.get("/", (req, res) => {
-  console.log(data);
   res.status(200).json(data);
 });
 
-server.post("/", (req, res) => {
-  newPerson = req.body;
-  data.push(newPerson)
-  res.status(200).json(newPerson);
-});
+// ------------------------------------------------------------------------------------------
 
-server.delete('/:id', (req, res) => {
-    const id = req.params.id
-    data.splice(id, 1)
-    res.status(200).json(data)
+server.get('/:id', (req, res) => {
+  const id = req.params.id;
+  const [selectedId] = data.filter( e => e.id == id)
+  if(selectedId){
+    res.status(200).json(selectedId)
+  } else {res.status(400).json({message: "Person could not be located."})}
 })
 
-server.put('/:id', (req, res) => {
-    const replacement = req.body
-    const id = req.params.id
-    data.splice(id, 1, {replacement})
-    res.status(200).json(data)
+// ------------------------------------------------------------------------------------------
+
+server.get('/:id/chores', (req,res)=>{
+  const id = req.params.id;
+  const completed = req.query.completed
+  let [selectedId] = data.filter( person => person.id == id);
+  if(selectedId){
+      if(completed == "true"){
+          let finishedChores = selectedId.chores.filter(chore => chore.completed === true)
+          res.status(200).json(finishedChores)
+      } else { res.status(200).json(selectedId.chores)}} 
+       else { res.status(400).json({message: "Person could not be located."})}
 })
 
+// ------------------------------------------------------------------------------------------
+
+
+server.delete('/:id/chores/:choreId', (req,res)=>{
+  const userId = req.params.id;
+  const choreId = req.params.choreId
+  let [selectedId] = data.filter( person => person.id == userId);
+  let newChores = selectedId.chores.filter(chore => chore.id != choreId)
+  if(selectedId){
+    selectedId.chores = [...newChores]
+    res.status(200).json(selectedId.chores)
+  } else {res.status(400).json({message: "Oh no! Couldn't find that user!"})}
+})
+// ------------------------------------------------------------------------------------------
+
+server.post('/:id/chores', (req,res)=>{
+  const id = req.params.id;
+  const body = req.body;
+  let [selectedId] = data.filter( person => person.id == id);
+  if(!body.assignedTo) {
+      res.status(404).json({message: "Assignto not defined"})
+  } else if(selectedId){
+    selectedId.chores.push(body);
+      res.status(200).json(selectedId.chores)
+  } else {res.status(400).json({message: "Person could not be located."})  }
+})
+
+// ------------------------------------------------------------------------------------------
+
+server.put('/:id/chores', (req,res)=>{
+  const id = req.params.id;
+  const body = req.body;
+  let [selectedId] = data.filter( person => person.id == id);
+  if(!body.assignedTo && !body.id) {
+      res.status(404).json({message: "Assignto or ID not defined "})
+  } else if(selectedId){
+     let newChores = selectedId.chores.filter(chore => chore.id != body.id);
+     selectedId.chores = [...newChores, body]
+      res.status(200).json(selectedId.chores)
+  }  else {res.status(400).json({message: "Person could not be located."}) }
+})
+
+
+// ------------------------------------------------------------------------------------------
 
 server.listen(8000, () => {
   console.log("API Running");
