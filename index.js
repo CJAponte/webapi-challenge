0f1b58b2092317daea8f735c2775097361cfb220
Cesar Aponte <cesar.aponte.1996.vazquez@gmail.com>
2019-08-09 18:07:23 -0400

    deploying

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ server.put('/:id/chores', (req,res)=>{
 
 
 // ------------------------------------------------------------------------------------------
-
-server.listen(8000, () => {
+const port = process.env.PORT || 8000;
+server.listen(port, () => {
   console.log("API Running");
 });
